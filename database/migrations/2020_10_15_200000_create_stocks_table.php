2a843afe63daa63643dd0a5ef31021bb392a10b6
webking <webking530@gmail.com>
2018-02-17 00:02:00 +1000

    Update

---
+++ database/migrations/2020_10_15_200000_create_stocks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->string('comment');
+            $table->foreignId('type_id')->constrained();
+            $table->integer('quantity');
+            $table->foreignId('size_id')->constrained();
+            $table->enum('addable', ['0', '1'])->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
--- helper.js
+++ helper.js
@@ -138,3 +138,5 @@ Contribution: 2018-02-17 00:00
 
 Contribution: 2018-02-17 00:01
 
+Contribution: 2018-02-17 00:02
+
